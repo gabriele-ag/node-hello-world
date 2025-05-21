af3ac062786dd5eb3ed60081bff9a625aab252cd
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-21 14:54:53 +0200

    feat: end first part exercise init bonus

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+import chalk from "chalk"
+// console.log(chalk.grey("Hello World"));
+
+
+// let frase;
+// if (process.argv[2]) {
+//     frase = process.argv[2]
+// } else {
+//     frase = "Hello Boolean"
+// }
+
+// Provo con il ternario
+
+const frase = process.argv[2]? process.argv[2] : "Hello Boolean";
+
+console.log(chalk.cyan(frase));
+
